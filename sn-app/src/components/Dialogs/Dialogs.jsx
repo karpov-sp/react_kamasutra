1dcdf2fee64c9726ccaca5ac72074edf02d4fbcd
KarpSP <sergej.p.karpov@gmail.com>
2022-12-12 19:47:17 +0300

    dialogs.jsx

--- sn-app/src/components/Dialogs/Dialogs.jsx
+++ sn-app/src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,38 @@ import classes from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    <img className={classes.pic} src="https://a.d-cd.net/kAAAAgIEteA-960.jpg" alt="avatar"/>
+                    <p className={classes.name}>Julia</p>
+                </div>
+                <div className={classes.dialog}>
+                    <img className={classes.pic} src="https://a.d-cd.net/kAAAAgIEteA-960.jpg" alt="avatar"/>
+                    <p className={classes.name}>Lena</p>
+                </div>
+                <div className={classes.dialog}>
+                    <img className={classes.pic} src="https://a.d-cd.net/kAAAAgIEteA-960.jpg" alt="avatar"/>
+                    <p className={classes.name}>Kate</p>
+                </div>
+                <div className={classes.dialog}>
+                    <img className={classes.pic} src="https://a.d-cd.net/kAAAAgIEteA-960.jpg" alt="avatar"/>
+                    <p className={classes.name}>Vania</p>
+                </div>
+                <div className={classes.dialog}>
+                    <img className={classes.pic} src="https://a.d-cd.net/kAAAAgIEteA-960.jpg" alt="avatar"/>
+                    <p className={classes.name}>Sofi</p>
+                </div>
+                <div className={classes.dialog}>
+                    <img className={classes.pic} src="https://a.d-cd.net/kAAAAgIEteA-960.jpg" alt="avatar"/>
+                    <p className={classes.name}>Matvei</p>
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>How are you?</div>
+                <div className={classes.message}>Yo</div>
+            </div>
         </div>
     )
 }
